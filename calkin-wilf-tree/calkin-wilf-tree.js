1f35cc671b4d3a327d3a3aafcd28b47413442e4b
Varun Vachhar <varunvachhar@gmail.com>
2018-09-25 12:21:35 -0400

    calkin-wilf-tree

--- calkin-wilf-tree/calkin-wilf-tree.js
+++ calkin-wilf-tree/calkin-wilf-tree.js
@@ -3,27 +3,19 @@ const chroma = require('chroma-js');
 const { randomNumber, range } = require('../math');
 const Tree = require('./tree');
 
+const DEPTH = 6;
+
 const settings = {
   animate: true,
-  duration: 20,
+  duration: DEPTH + 2,
   dimensions: [800, 600],
   scaleToView: true,
   playbackRate: 'throttle',
-  fps: 24,
+  fps: 1,
 };
 
-const DEPTH = 6;
-const steps = range(6).reduce((acc, idx) => ({ ...acc, [acc[idx]]: [] }), {});
-
-// prettier-ignore
-const clrs = chroma.cubehelix()
-  .start(randomNumber(0, 360))
-  .rotations(-0.35)
-  .gamma(0.7)
-  .lightness([0.4, 0.8])
-.scale()
-  .correctLightness()
-  .colors(DEPTH + 2);
+let steps;
+let clrs;
 
 // Each vertex a/b has two children:
 // (a+b)/b and a/(a+b)
@@ -33,18 +25,20 @@ function fractions([a, b]) {
 
 function drawCalkinWilf(context) {
   return function calkinWilf([w, h], [a, b], direction, [x, y], [f, l], d) {
-    // Draw Fraction
-    context.fillStyle = clrs[d];
-    context.font = `${f}px monospace`;
-    context.fillText(`${a}/${b}`, x, y);
-    // Draw Branches
-    if (direction === 'HORIZONTAL') {
-      drawLine('LEFT', context, [x, y], [w, h], [f, l]);
-      drawLine('RIGHT', context, [x, y], [w, h], [f, l]);
-    } else {
-      drawLine('TOP', context, [x, y], [w, h], [f, l]);
-      drawLine('BOTTOM', context, [x, y], [w, h], [f, l]);
-    }
+    steps[d].push(() => {
+      // Draw Fraction
+      context.fillStyle = clrs[d];
+      context.font = `${f}px monospace`;
+      context.fillText(`${a}/${b}`, x, y);
+      // Draw Branches
+      if (direction === 'HORIZONTAL') {
+        drawLine('LEFT', context, [x, y], [w, h], [f, l]);
+        drawLine('RIGHT', context, [x, y], [w, h], [f, l]);
+      } else {
+        drawLine('TOP', context, [x, y], [w, h], [f, l]);
+        drawLine('BOTTOM', context, [x, y], [w, h], [f, l]);
+      }
+    });
 
     if (d < DEPTH) {
       const [nA, nB] = fractions([a, b]);
@@ -66,17 +60,30 @@ function drawCalkinWilf(context) {
 }
 
 canvasSketch(() => {
-  // console.clear();
-
   return {
-    render({ context, frame, width, height, playhead }) {},
-    begin({ context, width, height }) {
-      context.clearRect(0, 0, width, height);
-      context.fillStyle = '#111322';
-      context.fillRect(0, 0, width, height);
+    render({ context, frame, width, height, playhead, time }) {
+      const activeStep = Math.floor(time) - 1;
+
+      clrs = palette();
       context.textAlign = 'center';
       context.textBaseline = 'middle';
       context.strokeStyle = clrs[0];
+
+      steps[activeStep] &&
+        steps[activeStep].forEach(node => {
+          node();
+        });
+    },
+    begin({ context, width, height }) {
+      clrs = palette();
+      steps = range(DEPTH + 1).reduce(
+        (acc, idx) => ({ ...acc, [idx]: [] }),
+        {},
+      );
+
+      context.clearRect(0, 0, width, height);
+      context.fillStyle = '#111322';
+      context.fillRect(0, 0, width, height);
 
       drawCalkinWilf(context)(
         [width / 4, (height * 1.5) / 4],
@@ -94,6 +101,7 @@ function drawLine(direction, context, [x, y], [w, h], [f, l]) {
   const xPad = f * 1.25;
   const yPad = f * 0.75;
 
+  context.beginPath();
   if (direction === 'LEFT') {
     context.moveTo(x - xPad, y);
     context.lineTo(x - w + xPad, y);
@@ -110,4 +118,17 @@ function drawLine(direction, context, [x, y], [w, h], [f, l]) {
 
   context.lineWidth = l;
   context.stroke();
+}
+
+function palette() {
+  // prettier-ignore
+  return chroma
+    .cubehelix()
+    .start(randomNumber(0, 360))
+    .rotations(-0.35)
+    .gamma(0.7)
+    .lightness([0.4, 0.8])
+  .scale()
+    .correctLightness()
+    .colors(DEPTH + 2);
 }
