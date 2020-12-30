3eadcea2b7c67d5062fe985e1b32a73eb26ac2c3
winkerVSbecks <varunvachhar@gmail.com>
2020-12-30 16:53:35 -0500

    shards

--- shatter.js
+++ shatter.js
@@ -1,144 +1,125 @@
 const canvasSketch = require('canvas-sketch');
-const { linspace, lerp, lerpArray } = require('canvas-sketch-util/math');
+const { linspace, lerpArray } = require('canvas-sketch-util/math');
+const eases = require('eases');
 const Random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [2048, 2048],
+  animate: true,
+  duration: 2,
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
+const sketch = ({ width, height }) => {
+  const R = width / 4;
+  const center = [width / 2, height / 2 + R / 4];
+
+  const [a, b, c] = triangle(center, R);
+
+  let t1 = 0;
+  let t2 = 0;
+  let t3 = 0;
+  let t4 = 0;
+  let paths = [];
+  let state = 'ab';
+  let from = { t1, t2 };
+
+  while (t3 < 1) {
+    const newState = nextState(state, t1, t2, t3, t4, [a, b, c], from);
+    paths.push(newState.path);
+    state = newState.state;
+    t1 = newState.t1;
+    t2 = newState.t2;
+    t3 = newState.t3;
+    t4 = newState.t4;
+    from = newState.from;
+  }
+
+  return ({ context, width, height, playhead }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     context.lineWidth = 10;
     context.lineJoin = 'round';
 
-    const R = width / 4;
-    const center = [width / 2, height / 2 + R / 4];
-
-    const [a, b, c] = triangle(center, R);
-
     context.strokeStyle = '#ccc';
-    drawPath(context, [a, b, c]);
+    // drawPath(context, [a, b, c]);
 
-    let k = 0;
-    while (k < 1) {
-      const [x1, y1] = lerpArray(a, c, k);
-      const [x2, y2] = lerpArray(a, b, k + 0.05);
-      k += 0.05;
-
-      context.fillStyle = 'black';
-      context.beginPath();
-      context.fillRect(x1 - 2, y1 - 2, 4, 4);
-      context.fillRect(x2 - 2, y2 - 2, 4, 4);
-    }
-
-    const done = false;
-    let t1 = 0;
-    let t2 = 0;
-    let t3 = 0;
-    let t4 = 0;
-    let nextA, nextB;
-    let paths = [];
-    let state = 'ab';
-    let count = 0;
-
-    while (t1 < 1) {
-      // t1 = Random.range(t1, Math.min(t1 + 0.25, 1));
-      count++;
-
-      const newState = nextState(state, t1, t2, nextA, nextB, [a, b, c]);
-      paths.push(newState.path);
-      state = newState.state;
-      t1 = newState.t1;
-      t2 = newState.t2;
-      nextA = newState.nextA;
-      nextB = newState.nextB;
-
-      // if (state === 'ab') {
-      //   t2 = Random.range(t2, t2 + 0.5);
-      //   var u = lerpArray(a, c, t1);
-      //   var v = lerpArray(a, b, t2);
-      // } else {
-      //   t2 = Random.range(t2, t2 + 0.5);
-      //   var u = lerpArray(a, c, t1);
-      //   var v = lerpArray(b, c, t2);
-      // }
-
-      // if (nextA && nextB) {
-      //   paths.push([nextA, nextB, v, u]);
-      // } else {
-      //   paths.push([a, u, v]);
-      // }
-
-      // const t1Original = t1;
-      // const t2Original = t2;
-      // t1 = Math.min(t1 + 0.075, 1);
-      // t2 = (t1 * t2) / t1Original;
-
-      // if (state === 'ab') {
-      //   nextB = lerpArray(a, b, t2);
-      //   nextA = lerpArray(a, c, t1);
-      // } else {
-      //   nextB = lerpArray(b, c, t2);
-      //   nextA = lerpArray(a, c, t1);
-      // }
-
-      // if (t2 === 1) {
-      //   state = 'bc';
-      //   // const t1Original = t1;
-      //   // const t2Original = t2;
-
-      //   // t1 = t1 + 0.075;
-      //   // t2 = (t1 * t2) / t1Original;
-      // }
-    }
-
-    context.strokeStyle = 'black';
+    context.strokeStyle = '#333';
+    context.fillStyle = '#333';
+    const pingPonPlayhead = Math.sin(Math.PI * playhead);
+    const t = eases.expoInOut(pingPonPlayhead);
     paths.forEach((path) => {
-      drawPath(context, path);
+      const p = path.from.map((pt, idx) => lerpArray(pt, path.to[idx], t));
+
+      drawPath(context, p);
+      context.fill();
     });
   };
 };
 
 canvasSketch(sketch, settings);
 
-function nextState(state, t1, t2, nextA, nextB, [a, b, c]) {
-  let path;
+function nextState(state, t1, t2, t3, t4, [a, b, c], from) {
+  let path,
+    wraps = false;
 
-  t1 = Math.min(Random.range(t1, t1 + 0.25), 1);
-  t2 = Math.min(Random.range(t2, t2 + 0.5), 1);
+  t3 = Math.min(Random.range(t1, t1 + 0.25), 1);
+  t3 = t3 > 0.95 ? 1 : t3;
+  t4 =
+    state === 'ab'
+      ? Random.range(t2, t2 + 0.75)
+      : Math.min(Random.range(t2, t2 + 0.25), 1);
+
+  if (t3 === 1) {
+    t4 = 1;
+  }
+
   const u = lerpArray(a, c, t1);
-
-  if (t2 === 1) {
-    state = 'bc';
-    t2 = 0;
-  }
-
+  const fromU = lerpArray(a, c, from.t1);
   const v = state === 'ab' ? lerpArray(a, b, t2) : lerpArray(b, c, t2);
+  const fromV =
+    state === 'ab' ? lerpArray(a, b, from.t2) : lerpArray(b, c, from.t2);
 
-  if (nextA && nextB) {
-    path = [nextA, nextB, v, u];
-  } else {
-    path = [a, u, v];
+  if (t2 <= 1 && t4 > 1) {
+    state = 'bc';
+    wraps = true;
+    t4 = Random.range(0.1, 0.25);
   }
 
-  const t1Original = t1;
-  t1 = Math.min(t1 + 0.05, 1);
-  t2 = (t1 * t2) / t1Original;
+  const w = state === 'ab' ? lerpArray(a, b, t4) : lerpArray(b, c, t4);
+  const x = lerpArray(a, c, t3);
 
-  nextA = lerpArray(a, c, t1);
-  nextB = state === 'ab' ? lerpArray(a, b, t2) : lerpArray(b, c, t2);
+  const pathFrom = wraps ? [fromU, fromV, b, w, x] : [fromU, fromV, w, x];
+  const pathTo = wraps ? [u, v, b, w, x] : [u, v, w, x];
+
+  path = { from: pathFrom, to: pathTo };
+
+  from = { t1: t3, t2: t4 };
+
+  t1 = Math.min(t3 + 0.05, 1);
+  t2 = Math.min(nextT2(state, t1, t3, t4), 1);
 
   return {
     state,
     path,
     t1,
     t2,
-    nextA,
-    nextB,
+    t3,
+    t4,
+    from,
   };
+}
+
+function nextT2(state, t1, t3, t4) {
+  if (state === 'ab') {
+    return (t4 * t1) / t3;
+  }
+
+  const t1Prime = 1 - t1;
+  const t3Prime = 1 - t3;
+  const t4Prime = 1 - t4;
+
+  return 1 - (t4Prime * t1Prime) / t3Prime;
 }
 
 function triangle([x, y], R) {
