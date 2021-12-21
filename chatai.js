22229b09f6fa07c68abb234109c03ed3bb1f091d
winkerVSbecks <varunvachhar@gmail.com>
2021-12-21 00:54:59 -0500

    chatai quadtree

--- chatai.js
+++ chatai.js
@@ -8,87 +8,103 @@ const d3 = require('d3-quadtree');
 const settings = {
   dimensions: [2048, 2048],
   animate: true,
-  // duration: 12,
-  // fps: 24,
+  // duration: 6,
+  // fps: 6,
   // playbackRate: 'throttle',
 };
 
-const colorConfig = {
-  total: 9,
-  centerHue: Random.range(0, 360),
-  hueCycle: 1,
-  curveMethod: 'lamé',
-  curveAccent: 0.2,
-  offsetTint: 0.251,
-  offsetShade: 0.01,
-  tintShadeHueShift: 0.0,
-  offsetCurveModTint: 0.03,
-  offsetCurveModShade: 0.03,
-  minSaturationLight: [0.5, 0.5],
-  maxSaturationLight: [1, 1],
-};
-
-const colorSystem = generateRandomColorRamp(colorConfig);
-
-const darkColorSystem = generateRandomColorRamp({
-  ...colorConfig,
-  maxSaturationLight: [0.25, 0.25],
-});
-
-const hsl = (c) => `hsla(${c[0]}, ${c[1] * 100}%, ${c[2] * 100}%, 1)`;
-const bg = hsl(Random.pick(darkColorSystem.dark));
-const inkColors = colorSystem.light.map(hsl).filter((c) => c !== bg);
-const clrs = {
-  bg,
-  ink: () => Random.pick(inkColors),
-};
-
 const config = {
-  resolution: 120,
+  resolution: 90,
   size: 5,
-  walkerCount: Random.rangeFloor(20, 40),
-  colors: {
-    background: clrs.bg,
-    grid: clrs.ink(),
-  },
+  walkerCount: 4, //Random.rangeFloor(20, 40),
+  colors: createColors(),
+  margin: 0,
 };
 
 const state = {
   grid: [],
   walkers: [],
   pts: [],
+  nodes: [],
+  activeNode: 0,
+  activeColor: config.colors.ink(),
+  mode: 'draw',
 };
 
 const sketch = () => {
   return {
     begin({ width, height }) {
+      state.pts =
+        state.pts.length > 0
+          ? state.pts
+          : new Array(25)
+              .fill(null)
+              .map(() => [
+                Random.rangeFloor(0, config.resolution),
+                Random.rangeFloor(0, config.resolution),
+              ]);
+      state.nodes =
+        state.nodes.length > 0
+          ? state.nodes
+          : quadTreeToNodes(state.pts, width, height);
       state.grid = makeGrid();
       state.walkers = new Array(config.walkerCount).fill(null).map(makeWalker);
-      state.pts = new Array(2500)
-        .fill(null)
-        .map(() => [Random.rangeFloor(0, width), Random.rangeFloor(0, height)]);
     },
-    render({ context, width, height }) {
+    render({ context, width, height, playhead }) {
       // clear
       context.clearRect(0, 0, width, height);
       context.fillStyle = config.colors.background;
       context.fillRect(0, 0, width, height);
 
-      drawGrid(context, state.grid, width, height);
+      // context.lineWidth = 2;
+      // context.strokeStyle = config.colors.grid;
+      // state.nodes.forEach((node) => {
+      //   context.strokeRect(
+      //     toWorld(node.x, width),
+      //     toWorld(node.y, height),
+      //     toWorld(node.width, width),
+      //     toWorld(node.height, height)
+      //   );
+      // });
 
-      state.walkers.forEach((walker) => {
-        if (walker.state === 'alive') {
-          step(walker);
+      context.lineWidth = 8;
+      const node = state.nodes[state.activeNode];
+      context.strokeStyle = config.colors.lines;
+      context.strokeRect(
+        toWorld(node.x, width),
+        toWorld(node.y, height),
+        toWorld(node.width, width),
+        toWorld(node.height, height)
+      );
+
+      for (let index = 0; index < 100; index++) {
+        drawGrid(context, state.grid, width, height);
+
+        state.walkers.forEach((walker) => {
+          if (walker.state === 'alive') {
+            step(walker);
+          }
+          drawWalker(context, walker, width, height);
+        });
+
+        const validOptions = state.grid
+          .filter((cell) => !cell.occupied)
+          .filter((cell) => inNode(cell));
+
+        // if (state.walkers.length === 0) {
+        //   spawnWalker();
+        // }
+
+        if (
+          validOptions.length === 0 &&
+          state.activeNode < state.nodes.length - 1
+        ) {
+          state.activeNode++;
+          spawnWalker();
+        } else if (state.activeNode === state.nodes.length - 1) {
+          state.mode = 'complete';
         }
-        drawWalker(context, walker, width, height);
-      });
-
-      const nodes = quadtreeToNodes(state.pts, width, height);
-      context.lineWidth = 2;
-      nodes.forEach((node) => {
-        context.strokeStyle = config.colors.grid;
-        context.strokeRect(node.x, node.y, node.width, node.height);
-      });
+      }
     },
   };
 };
@@ -160,9 +176,7 @@ const walkerTypes = [
 ];
 
 function spawnWalker() {
-  const doSpawn = !state.grid.every((cell) => cell.occupied);
-
-  if (doSpawn) {
+  if (state.mode !== 'complete') {
     const walker = makeWalker();
 
     if (walker) {
@@ -175,11 +189,12 @@ function makeWalker() {
   const start = getStart();
 
   if (start) {
-    (start.moveTo = true), setOccupied(start);
+    start.moveTo = true;
+    setOccupied(start);
 
     return {
       path: [start],
-      color: clrs.ink(),
+      color: config.colors.ink(),
       state: 'alive',
       nextStep: Random.pick(walkerTypes)(),
     };
@@ -188,7 +203,11 @@ function makeWalker() {
 }
 
 function getStart() {
-  return Random.pick(state.grid.filter((cell) => !cell.occupied));
+  const options = state.grid
+    .filter((cell) => !cell.occupied)
+    .filter((cell) => inNode(cell));
+
+  return Random.pick(options);
 }
 
 function step(walker) {
@@ -203,19 +222,6 @@ function step(walker) {
     walker.state = 'dead';
     spawnWalker();
   }
-}
-
-function findNextStep({ x, y }) {
-  const options = [
-    { x: x + 1, y: y },
-    { x: x - 1, y: y },
-    { x: x, y: y + 1 },
-    { x: x, y: y - 1 },
-  ].filter((potentialNext) => {
-    return inBounds(potentialNext) && !isOccupied(potentialNext);
-  });
-
-  return Random.pick(options);
 }
 
 function drawWalker(context, walker, width, height) {
@@ -276,7 +282,7 @@ function setOccupied({ x, y }) {
 }
 
 function validOption(option) {
-  return inBounds(option) && !isOccupied(option);
+  return inBounds(option) && inNode(option) && !isOccupied(option);
 }
 
 /**
@@ -291,29 +297,42 @@ function inBounds({ x, y }) {
   return x >= 0 && x <= config.resolution && y >= 0 && y <= config.resolution;
 }
 
+function inNode({ x, y }) {
+  const node = state.nodes[state.activeNode];
+
+  return (
+    x >= node.x &&
+    x <= node.x + node.width &&
+    y >= node.y &&
+    y <= node.y + node.height
+  );
+}
+
 function xyToCoords(x, y, width, height) {
   return [toWorld(x, width), toWorld(y, height)];
 }
 
 function toWorld(v, size) {
-  const margin = 0.03 * size;
+  const margin = config.margin * size;
   return mapRange(v, 0, config.resolution, margin, size - margin);
 }
 
 /**
  * Quadtree
  */
-function quadtreeToNodes(pts, width, height) {
+function quadTreeToNodes(pts, width, height) {
   const quadtree = d3
     .quadtree()
     .extent([
       [0, 0],
-      [width, height],
+      [config.resolution, config.resolution],
+      // [config.margin * width, config.margin * height],
+      // [width - config.margin * width, height - config.margin * height],
     ])
     .addAll(pts);
 
   const nodes = [];
-  quadtree.visit((node, x0, y0, x1, y1) => {
+  quadtree.visitAfter((node, x0, y0, x1, y1) => {
     nodes.push({
       x: x0,
       y: y0,
@@ -326,76 +345,40 @@ function quadtreeToNodes(pts, width, height) {
   return nodes;
 }
 
+function createColors() {
+  const colorConfig = {
+    total: 9,
+    centerHue: Random.range(0, 360),
+    hueCycle: 1,
+    curveMethod: 'lamé',
+    curveAccent: 0.2,
+    offsetTint: 0.251,
+    offsetShade: 0.01,
+    tintShadeHueShift: 0.0,
+    offsetCurveModTint: 0.03,
+    offsetCurveModShade: 0.03,
+    minSaturationLight: [0, 0],
+    maxSaturationLight: [1, 1],
+  };
+
+  const colorSystem = generateRandomColorRamp(colorConfig);
+
+  const darkColorSystem = generateRandomColorRamp({
+    ...colorConfig,
+    maxSaturationLight: [0.25, 0.25],
+  });
+
+  const hsl = (c) => `hsla(${c[0]}, ${c[1] * 100}%, ${c[2] * 100}%, 1)`;
+  const background = hsl(Random.pick(darkColorSystem.dark));
+  const inkColors = colorSystem.light.map(hsl).filter((c) => c !== background);
+  const ink = () => Random.pick(inkColors);
+
+  return {
+    background,
+    ink,
+    grid: ink(),
+    lines: ink(),
+  };
+}
+
 canvasSketch(sketch, settings);
-
-// const walkerTypes = [
-//   ({ x, y }) =>
-//     Random.pick(
-//       [
-//         { x: x + 1, y: y },
-//         { x: x - 1, y: y },
-//         { x: x, y: y + 1 },
-//         { x: x, y: y - 1 },
-//       ].filter(validOption)
-//     ),
-//   ({ x, y }) => {
-//     const preferred = { x: x + 1, y: y };
-
-//     if (validOption(preferred)) {
-//       return preferred;
-//     }
-
-//     return Random.pick(
-//       [
-//         { x: x - 1, y: y },
-//         { x: x, y: y + 1 },
-//         { x: x, y: y - 1 },
-//       ].filter(validOption)
-//     );
-//   },
-//   ({ x, y }) => {
-//     const preferred = { x: x - 1, y: y };
-
-//     if (validOption(preferred)) {
-//       return preferred;
-//     }
-
-//     return Random.pick(
-//       [
-//         { x: x + 1, y: y },
-//         { x: x, y: y + 1 },
-//         { x: x, y: y - 1 },
-//       ].filter(validOption)
-//     );
-//   },
-//   ({ x, y }) => {
-//     const preferred = { x: x, y: y + 1 };
-
-//     if (validOption(preferred)) {
-//       return preferred;
-//     }
-
-//     return Random.pick(
-//       [
-//         { x: x + 1, y: y },
-//         { x: x - 1, y: y },
-//         { x: x, y: y - 1 },
-//       ].filter(validOption)
-//     );
-//   },
-//   ({ x, y }) => {
-//     const preferred = { x: x, y: y - 1 };
-
-//     if (validOption(preferred)) {
-//       return preferred;
-//     }
-
-//     return Random.pick(
-//       [
-//         { x: x + 1, y: y },
-//         { x: x - 1, y: y },
-//         { x: x, y: y + 1 },
-//       ].filter(validOption)
-//     );
-//   },
-// ];
